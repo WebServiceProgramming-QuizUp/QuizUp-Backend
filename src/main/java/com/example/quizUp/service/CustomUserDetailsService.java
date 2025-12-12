90ffb64dc04d1c88447a46c340de78a6daeddc0a
junmin <coom1222@gmail.com>
2025-12-13 00:41:04 +0900

    이전 커밋에 반영 안된 파일 추가

---
+++ src/main/java/com/example/quizUp/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.quizUp.service;
+
+import com.example.quizUp.domain.User;
+import com.example.quizUp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + userId));
+    }
+}
